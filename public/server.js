d9e518d8de37fc8f50fd8e7b1e140b4a1e9cb05e
Peter Krak <pkrak@csas.cz>
2018-07-18 14:55:36 +0200

    .
    .

---
+++ public/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require ('path');
+
+var app = express();
+
+app.use(express.static(__dirname ));
+
+
+app.get('/', function (req, res) {
+    res.sendFile('Card.html', {root: path.join(__dirname, './html')});
+});
+
+app.get('/test', function (req, res) {
+    res.sendFile('test.html', {root: path.join(__dirname, './html')});
+});
+
+
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var ip   = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
+
+app.listen(port, ip, function () {
+    console.log('Server running on http://%s:%s', ip, port);
+});
